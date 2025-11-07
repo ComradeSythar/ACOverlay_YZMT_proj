dfc71468c5e3a541f297e9f63783a28b2ef84b9f
ComradeSythar <sythara@salrun.com>
2025-11-06 20:58:57 -0700

    Create PPERequesterRegistrations.c

---
+++ ACOverlay_YZMT/scripts/3_Game/ppemanager/PPERequesterRegistrations.c
@@ -0,0 +1,9 @@
+modded class PPERequesterRegistrations
+{
+    override void RegisterGameplayRequesters()
+    {
+        super.RegisterGameplayRequesters();
+
+        RegisterRequester(PPERequester_ACOverlay_YZMT);
+    }
+}
